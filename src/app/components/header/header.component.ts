65e16f45fc19ecf6bff57e4aa409e56fe36608e7
VinicioSales <vinicio.santtana@gmail.com>
2023-08-29 13:35:05 -0300

    Estilizando header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.pageYOffset;
+    const scrollTop = yOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollTop > 100) { // algumValor é o valor de scroll que você quer para a mudança acontecer
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
