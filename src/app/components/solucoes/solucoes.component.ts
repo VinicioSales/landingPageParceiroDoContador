4a4c405ef720b3f178f7fc0fe8a1751d9151c4a2
VinicioSales <vinicio.santtana@gmail.com>
2023-08-30 22:25:35 -0300

    Criando solucoes

--- src/app/components/solucoes/solucoes.component.ts
+++ src/app/components/solucoes/solucoes.component.ts
@@ -7,6 +7,6 @@ import { CarouselComponent } from 'ngx-bootstrap/carousel';
   styleUrls: ['./solucoes.component.css']
 })
 export class SolucoesComponent {
-
-
+  descricao: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  tituloSolucao: string = 'Automações'
 }
