1821631caf29b9da8ba39883b1148e6eae5ffb08
VinicioSales <vinicio.santtana@gmail.com>
2023-09-05 09:29:45 -0300

    Juntando solucoes com carrossel

--- src/app/components/solucoes/solucoes.component.ts
+++ src/app/components/solucoes/solucoes.component.ts
@@ -11,4 +11,10 @@ export class SolucoesComponent {
   @Input() imagem2!: string;
   @Input() descricao!: string;
   @Input() tituloSolucao!: string;
+
+  solucoes = [
+    // {tituloSolucao: 'Automações', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', imagem1: 'assets/img/engrenagem.png', imagem2: 'assets/img/cerebro.png'},
+    // {tituloSolucao: 'Integrações', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', imagem1: 'assets/img/integracao.png', imagem2: 'assets/img/integracao2.png'},
+    {tituloSolucao: 'Análise de Dados', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', imagem1: 'assets/img/dados.png', imagem2: 'assets/img/dados2.png'},
+  ]
 }
