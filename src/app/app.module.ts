6dbbd0293d79e87b57c3eac9270617c6d0f3eb66
VinicioSales <vinicio.santtana@gmail.com>
2023-09-12 11:21:55 -0300

    add submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {MatMenuModule} from '@angular/material/menu';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
-import { UsbComponent } from './components/usb/usb.component';
-import { SobreComponent } from './components/sobre/sobre.component';
-import { SolucoesComponent } from './components/solucoes/solucoes.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { BotaoComponent } from './components/botao/botao.component';
-import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { ModalSolucoesComponent } from './components/modal-solucoes/modal-solucoes.component';
-import { Botao2Component } from './components/botao2/botao2.component'
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { UsbComponent } from './components/usb/usb.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { BotaoComponent } from './components/botao/botao.component';
+import { Botao2Component } from './components/botao2/botao2.component'
+import { HeaderComponent } from './components/header/header.component';
+import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { SolucoesComponent } from './components/solucoes/solucoes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormularioComponent } from './components/formulario/formulario.component'; 
+import { ModalSolucoesComponent } from './components/modal-solucoes/modal-solucoes.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +41,8 @@ import { FormularioComponent } from './components/formulario/formulario.componen
     CarouselModule.forRoot(),
     NgbModule,
     NgbCarouselModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, this.noNumberValidator]],
+      empresa: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required, this.phoneValidator]],
+      descricao: ['', Validators.required]
+    });
+  }
+
+  noNumberValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    console.log("noNumberValidator");
+    const hasNumber = /\d/.test(control.value);
+    return hasNumber ? { 'hasNumber': true } : null;
+  }
+
+  phoneValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    console.log("phoneValidator");
+    const pattern = /^\(\d{3}\) \d \d{4}-\d{4}$/;
+    return !pattern.test(control.value) ? { 'invalidPhone': true } : null;
+  }
+
+  onSubmit() {
+    console.log("onSubmit");
+    if (this.form.valid) {
+      const formData = this.form.value;
+      console.log(formData);
+      // Processe os dados do formulário aqui
+    }
+  }
 }
