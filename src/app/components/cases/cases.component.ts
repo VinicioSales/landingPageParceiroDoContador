88e8b139d61c375fc3407b67b85549bac8cab931
Sajahh <mariavaicomoutras@hotmail.com>
2023-09-19 16:57:51 -0300

    att

--- src/app/components/cases/cases.component.ts
+++ src/app/components/cases/cases.component.ts
@@ -1,33 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { ApiService } from "src/app/services/cases/cases.service";
+import { Component } from "@angular/core";
 
 @Component({
 	selector: "app-cases",
 	templateUrl: "./cases.component.html",
 	styleUrls: ["./cases.component.css"],
 })
-export class CasesComponent implements OnInit {
-	imgCase1!: string;
-	imgCase2!: string;
-	imgCase3!: string;
-	textoCase1!: string;
-
-	cases: any[] = [];
-
-	constructor(private apiService: ApiService) {} // Injete o serviço
-
-	ngOnInit(): void {
-		this.fetchCases();
-	}
-
-	fetchCases(): void {
-		this.apiService.getCases().subscribe(
-			(data) => {
-				this.cases = data;
-			},
-			(error) => {
-				console.error("Erro ao buscar cases:", error);
-			}
-		);
-	}
+export class CasesComponent {
+	textoCase1: string =
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
+	imgCase1: string = "assets/img/dados.png";
+	imgCase2: string = "assets/img/dados.png";
+	imgCase3: string = "assets/img/dados.png";
 }
