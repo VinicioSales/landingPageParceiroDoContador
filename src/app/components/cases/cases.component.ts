408506c5a1bc7737a35e6027de420ac2721b7417
VinicioSales <vinicio.santtana@gmail.com>
2023-09-25 16:55:57 -0300

    descomentado cases teste

--- src/app/components/cases/cases.component.ts
+++ src/app/components/cases/cases.component.ts
@@ -14,19 +14,19 @@ export class CasesComponent implements OnInit {
 	imgCase2: string = "assets/img/dados.png";
 	imgCase3: string = "assets/img/dados.png";
 
-  casesData = [
-    {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-    {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-    {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-  ]
-  // casesData: any[] = [];
+  // casesData = [
+  //   {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  //   {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  //   {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  // ]
+  casesData: any[] = [];
 
-  // constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    // this.apiService.getCases().subscribe(data => {
-    //   this.casesData = data;
-    //   console.log(this.casesData);
-    // });
+    this.apiService.getCases().subscribe(data => {
+      this.casesData = data;
+      console.log(this.casesData);
+    });
   }
 }
