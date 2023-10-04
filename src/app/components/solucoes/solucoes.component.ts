96561b42aa89c5dd1d64bfbaa4035e79e330f778
VinicioSales <vinicio.santtana@gmail.com>
2023-10-04 14:10:45 -0300

    feat: Solucoes responsivo

--- src/app/components/solucoes/solucoes.component.ts
+++ src/app/components/solucoes/solucoes.component.ts
@@ -23,22 +23,22 @@ export class SolucoesComponent {
 			imagem2: "/assets/img/Ícones - Versão Azul-17.png",
 			buttonColor: 'var(--azul-claro)',
 		},
-		// {
-		// 	tituloSolucao: "INTEGRAÇÕES",
-		// 	descricao:
-		// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		// 	imagem1: "assets/img/Ícones - Versão Laranja-34.png",
-		// 	imagem2: "assets/img/Ícones - Versão Laranja-35.png",
-		// 	buttonColor: 'var(--amarelo)',
-		// },
-		// {
-		// 	tituloSolucao: "ANÁLISE DE DADOS",
-		// 	descricao:
-		// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		// 	imagem1: "assets/img/Ícones - Versão Verde-02.png",
-		// 	imagem2: "assets/img/Ícones - Versão Verde-23.png",
-		// 	buttonColor: 'var(--verde-claro)',
-		// },
+		{
+			tituloSolucao: "INTEGRAÇÕES",
+			descricao:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			imagem1: "assets/img/Ícones - Versão Laranja-34.png",
+			imagem2: "assets/img/Ícones - Versão Laranja-35.png",
+			buttonColor: 'var(--amarelo)',
+		},
+		{
+			tituloSolucao: "ANÁLISE DE DADOS",
+			descricao:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			imagem1: "assets/img/Ícones - Versão Verde-02.png",
+			imagem2: "assets/img/Ícones - Versão Verde-23.png",
+			buttonColor: 'var(--verde-claro)',
+		},
 	];
 
 	openModal(tituloSolucao: string) {
