a619235c1a26f806c4e23396bb8d1c7af100ce61
VinicioSales <vinicio.santtana@gmail.com>
2023-10-04 11:00:13 -0300

    feat: Cor do botão dinamico

--- src/app/components/solucoes/solucoes.component.ts
+++ src/app/components/solucoes/solucoes.component.ts
@@ -21,21 +21,24 @@ export class SolucoesComponent {
 				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 			imagem1: "assets/img/engrenagem.png",
 			imagem2: "assets/img/cerebro.png",
+			buttonColor: 'var(--azul-claro)',
 		},
-		// {
-		// 	tituloSolucao: "Integrações",
-		// 	descricao:
-		// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		// 	imagem1: "assets/img/integracao.png",
-		// 	imagem2: "assets/img/integracao2.png",
-		// },
-		// {
-		// 	tituloSolucao: "Análise de Dados",
-		// 	descricao:
-		// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		// 	imagem1: "assets/img/dados.png",
-		// 	imagem2: "assets/img/dados2.png",
-		// },
+		{
+			tituloSolucao: "Integrações",
+			descricao:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			imagem1: "assets/img/integracao.png",
+			imagem2: "assets/img/integracao2.png",
+			buttonColor: 'var(--amarelo)',
+		},
+		{
+			tituloSolucao: "Análise de Dados",
+			descricao:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			imagem1: "assets/img/dados.png",
+			imagem2: "assets/img/dados2.png",
+			buttonColor: 'var(--verde-claro)',
+		},
 	];
 
 	openModal(tituloSolucao: string) {
