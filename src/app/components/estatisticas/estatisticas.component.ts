23184d37140252a26b99530414b3bf70c999efa2
VinicioSales <vinicio.santtana@gmail.com>
2023-10-10 20:36:21 -0300

    config: ajustando para teste

--- src/app/components/estatisticas/estatisticas.component.ts
+++ src/app/components/estatisticas/estatisticas.component.ts
@@ -8,22 +8,28 @@ import { BACK_URL } from "src/app/config"
 	styleUrls: ["./estatisticas.component.css"],
 })
 export class EstatisticasComponent implements OnInit {
-	estatisticas: any;
+	// estatisticas: any;
 
+	
+	estatisticas =  {
+			nome: "70% de produtividade",
+			descricao: "descricao",
+			media: "assets/img/celular-1.png"
+	}
 	constructor(private http: HttpClient) {}
 
 	ngOnInit(): void {
-		this.fetchStatistics();
+		// this.fetchStatistics();
 	}
 
-	fetchStatistics(): void {
-		this.http.get(`${BACK_URL}/statistics`).subscribe(
-			(data: any) => {
-				this.estatisticas = data[0];
-			},
-			(error) => {
-				console.error("Erro ao buscar estatísticas:", error);
-			}
-		);
-	}
+	// fetchStatistics(): void {
+	// 	this.http.get(`${BACK_URL}/statistics`).subscribe(
+	// 		(data: any) => {
+	// 			this.estatisticas = data[0];
+	// 		},
+	// 		(error) => {
+	// 			console.error("Erro ao buscar estatísticas:", error);
+	// 		}
+	// 	);
+	// }
 }
