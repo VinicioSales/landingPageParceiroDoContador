e1d8c9ce401159c620ffeb63e92a36c17f5989e3
VinicioSales <vinicio.santtana@gmail.com>
2023-10-14 07:58:42 -0300

    feat: Add dados testes

--- src/app/components/cases/cases.component.ts
+++ src/app/components/cases/cases.component.ts
@@ -14,19 +14,19 @@ export class CasesComponent implements OnInit {
 	imgCase2: string = "assets/img/dados.png";
 	imgCase3: string = "assets/img/dados.png";
 
-  // casesData = [
-  //   {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-  //   {media: "assets/img/dados.png", mensagem: "A parceiro tem muita criatividade e propriedade quando buscamos soluções para um processo, muitas vezes que demanda tempo, lá na GO conseguimos buscar automaticamente no sistema do cliente informaçoes que demandavamos diariamente muito tempo para fazer através de extração e importação de planilhas e relatórios, isso facilitou o acesso a informações e a redução de erros no processo!"},
-  //   {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-  // ]
-  casesData: any[] = [];
+  casesData = [
+    {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+    {media: "assets/img/dados.png", mensagem: "A parceiro tem muita criatividade e propriedade quando buscamos soluções para um processo, muitas vezes que demanda tempo, lá na GO conseguimos buscar automaticamente no sistema do cliente informaçoes que demandavamos diariamente muito tempo para fazer através de extração e importação de planilhas e relatórios, isso facilitou o acesso a informações e a redução de erros no processo!"},
+    {media: "assets/img/dados.png", mensagem: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  ]
+  // casesData: any[] = [];
 
-  constructor(private apiService: ApiService) { }
+  // constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.apiService.getCases().subscribe(data => {
-      this.casesData = data;
-      console.log(this.casesData);
-    });
+    // this.apiService.getCases().subscribe(data => {
+    //   this.casesData = data;
+    //   console.log(this.casesData);
+    // });
   }
 }
